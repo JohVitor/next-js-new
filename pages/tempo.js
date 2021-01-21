dcd1a0191bf477f0825cbdffc8c7af0f3aefdfa5
João Vitor Gardizan <jvagardizan@gmail.com>
2021-01-20 22:05:52 -0300

    Página Tempo

---
+++ pages/tempo.js
@@ -0,0 +1,16 @@
+function Tempo () {
+
+    const dynamicDate = newDate();
+    const dynamicDateString = dynamicDate.toGMTString()
+
+
+    return (
+        <div>
+            {dynamicDateString} (dinâmico)
+        </div>
+    )
+
+
+}
+
+export default Tempo
